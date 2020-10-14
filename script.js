289869a7b5b2a59e76c72d0974303440504e3053
Adityan-compile <67089770+Adityan-compile@users.noreply.github.com>
2020-10-14 21:25:24 +0530

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+*
+* Author: Adityan
+* Github: https://github.com/Adityan-compile
+*
+*/
+
+
+function runCode() {
+    var Code = document.getElementById("code").value;
+    document.getElementById("output").srcdoc = Code;
+}
+
+function welcome() {
+    alert("Welcome to my online HTML editor");
+}
+
